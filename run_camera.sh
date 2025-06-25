19307ce1222394babc169adfcbafb13022a9ab39
bober <bober@epitech.eu>
2025-06-25 18:31:55 +0200

    [FIX] camera + algo fonctionnelle

--- run_camera.sh
+++ run_camera.sh
@@ -4,8 +4,12 @@ set -euo pipefail
 IMAGE="robocar-maskgen:latest"
 
 echo "Démarrage du container $IMAGE…"
-sudo docker run --rm --runtime nvidia --gpus all --privileged \
-  -v /dev/bus/usb:/dev/bus/usb \
-  -v ~/robocar/camera/data:/app/data \
-  -v ~/robocar/camera/cam_mask:/app/cam_mask \
-  robocar-maskgen:latest
+
+sudo docker run --rm --runtime nvidia \
+     --gpus all --privileged \
+     -v /dev/bus/usb:/dev/bus/usb \
+     -v ~/robocar/camera/data:/app/data \
+     -v ~/robocar/camera/cam_mask:/app/cam_mask \
+     -v /usr/local/cuda-10.2/targets/aarch64-linux/lib:/usr/local/cuda/lib64 \
+     -e NVIDIA_VISIBLE_DEVICES=all \
+     "$IMAGE"
--- run_drive.sh
+++ run_drive.sh
@@ -2,9 +2,12 @@
 set -euo pipefail
 
 IMAGE="robocar-drive:latest"
+HOST_CAM_DIR="$HOME/robocar/camera"
+CONTAINER_APP_DIR="/app"
 
 echo "Démarrage du container $IMAGE…"
 docker run --privileged \
   -e SDL_VIDEODRIVER=dummy \
   --device /dev/ttyACM0:/dev/ttyACM0 \
+  -v "$HOST_CAM_DIR":"$CONTAINER_APP_DIR/camera":ro \
   "$IMAGE"
