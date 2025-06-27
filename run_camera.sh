a7ca54235cc66d5d76ec7f9fa27beb9ea4320123
bober <bober@epitech.eu>
2025-06-27 19:17:02 +0200

    [FIX] refacto code

--- run_camera.sh
+++ run_camera.sh
@@ -8,7 +8,6 @@ echo "Démarrage du container $IMAGE…"
 sudo docker run --rm --runtime nvidia \
      --gpus all --privileged \
      -v /dev/bus/usb:/dev/bus/usb \
-     -v ~/robocar/camera/data:/app/data \
      -v ~/robocar/camera/cam_mask:/app/cam_mask \
      -v /usr/local/cuda-10.2/targets/aarch64-linux/lib:/usr/local/cuda/lib64 \
      -e NVIDIA_VISIBLE_DEVICES=all \
