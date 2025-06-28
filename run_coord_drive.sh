34f8cc83e7ef9fd2f3e96c8c041177afeae3db1e
arrty00m <samuel.gross@epitech.eu>
2025-06-28 12:51:28 +0200

    [FIX] sudo perm for the coord drive script

--- run_coord_drive.sh
+++ run_coord_drive.sh
@@ -6,7 +6,7 @@ HOST_CAM_DIR="$HOME/robocar/camera"
 CONTAINER_APP_DIR="/app"
 
 echo "Démarrage du container $IMAGE…"
-docker run --privileged \
+sudo docker run --privileged \
   -e SDL_VIDEODRIVER=dummy \
   --device /dev/ttyACM0:/dev/ttyACM0 \
   -v "$HOST_CAM_DIR":"$CONTAINER_APP_DIR/camera":ro \
